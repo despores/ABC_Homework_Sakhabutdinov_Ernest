ad8f0e5a8e1072523ae2fd3617e9731571522d01
despores <70171791+despores@users.noreply.github.com>
2021-10-24 22:57:06 +0300

    Add files via upload

---
+++ Program/Sakhabutdinov_ErnestHW2/cmake-build-debug/CMakeFiles/Sakhabutdinov_ErnestHW2.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sakhabutdinov_ErnestHW2.dir/Container.cpp.obj"
+  "CMakeFiles/Sakhabutdinov_ErnestHW2.dir/Cube.cpp.obj"
+  "CMakeFiles/Sakhabutdinov_ErnestHW2.dir/RightThetreadare.cpp.obj"
+  "CMakeFiles/Sakhabutdinov_ErnestHW2.dir/Sphere.cpp.obj"
+  "CMakeFiles/Sakhabutdinov_ErnestHW2.dir/main.cpp.obj"
+  "Sakhabutdinov_ErnestHW2.exe"
+  "Sakhabutdinov_ErnestHW2.exe.manifest"
+  "Sakhabutdinov_ErnestHW2.pdb"
+  "libSakhabutdinov_ErnestHW2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sakhabutdinov_ErnestHW2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
